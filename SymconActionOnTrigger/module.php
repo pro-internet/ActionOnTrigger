d14a68e7f0c4f5df8e4038bc30ffcc43a5c2434b
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-04-26 14:14:38 +0200

    no message

--- SymconActionOnTrigger/module.php
+++ SymconActionOnTrigger/module.php
@@ -69,8 +69,6 @@
 
             $this->mergeEvents();
 
-            $this->linkTresholdSensors();
-
             $this->checkTreshold(); 
 
             if ($this->doesExist($this->searchObjectByName("Schwellwert"))) {
@@ -194,11 +192,6 @@
 
         } 
 
-        protected function linkTresholdSensors () {
-
-            
-        }
-
         // Registriert alle Properties (8 Lampen und 6 Senoren) 
  
         protected function registAllProperties () {
@@ -261,8 +254,6 @@
             $this->checkDimVar();
 
             $this->mergeEvents();
-
-            $this->linkTresholdSensors();
 
             $this->checkTreshold(); 
 
@@ -1239,7 +1230,7 @@
             $this->checkProfile("Switch", 0, 0, 1, 5, 1, "", "", "");
             $this->checkProfile("LOM.Tl", 1, 0, 3600, 5, 1, "", " s", "Clock");
             $this->checkProfile("LOM.Wert.100", 1, 0, 100, 1, 0, "", " %", "Electricity");
-            $this->checkProfile("Sun", 1, 0, 120000, 1000, 0, "", " lx", "Intensity");
+            $this->checkProfile("Sun", 2, 0, 120000, 1000, 0, "", " lx", "Intensity");
             $this->checkProfile("Temperature_C", 1, 0, 120, 1, 0, "", " °C", "Temperature");
             $this->checkProfile("Temperature_F", 1, 0, 3600, 30, 0, "", " °F", "Temperature");
             $this->checkProfile("Wattage", 1, 0, 5000, 1, 0, "", " W", "Electricity");
